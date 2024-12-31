cbf6149d848291005d9719bfeb0e84a6205fc7e9
jacobkim9881 <jacobkim9881@gmail.com>
2024-12-31 20:57:55 +0900

    fix: transition animation does not work

--- src/pages/settings/Profile/Contacts/ContactMethodDetailsPage.tsx
+++ src/pages/settings/Profile/Contacts/ContactMethodDetailsPage.tsx
@@ -188,7 +188,6 @@ function ContactMethodDetailsPage({route}: ContactMethodDetailsPageProps) {
 
     // Replacing spaces with "hard spaces" to prevent breaking the number
     const formattedContactMethod = Str.isSMSLogin(contactMethod) ? formatPhoneNumber(contactMethod) : contactMethod;
-    const hasMagicCodeBeenSent = !!loginData.validateCodeSent;
     const isFailedAddContactMethod = !!loginData.errorFields?.addedLogin;
     const isFailedRemovedContactMethod = !!loginData.errorFields?.deletedLogin;
 
